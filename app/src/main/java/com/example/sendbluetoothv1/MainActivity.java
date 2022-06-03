336d2cb8c7f23391790128336cd05e539ff5a53e
AnasHouissa <AnasHouissa@gmail.com>
2022-06-03 18:40:06 +0100

    outgoing cnx

--- app/src/main/java/com/example/sendbluetoothv1/MainActivity.java
+++ app/src/main/java/com/example/sendbluetoothv1/MainActivity.java
@@ -5,15 +5,23 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -21,22 +29,35 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private BluetoothChatService mChatService = null;
+
+    /**
+     * Array adapter for the conversation thread
+     */
+
+    private String mConnectedDeviceName = null;
+
+
+    int REQUEST_CONNECT_DEVICE_SECURE = 1;
+    int REQUEST_ENABLE_BT = 12;
+
+    BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int REQUEST_ENABLE_BT = 12;
 
         EditText et_name = findViewById(R.id.et_name);
         Button btn_send = findViewById(R.id.btn_send);
+        Button btn_connect = findViewById(R.id.btn_connect);
 
         btn_send.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("MissingPermission")
             @Override
             public void onClick(View view) {
-                BluetoothManager bluetoothManager = getSystemService(BluetoothManager.class);
-                BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
+
                 // check if device supports bluetooth
                 if (bluetoothAdapter == null) {
                     Toast.makeText(MainActivity.this, "Device doesn't support bluetooth", Toast.LENGTH_SHORT).show();
@@ -48,12 +69,21 @@ public class MainActivity extends AppCompatActivity {
                         startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
 
                     } else {
-                        Toast.makeText(MainActivity.this, "show availaible ppl", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, "Access Granted", Toast.LENGTH_SHORT).show();
+                        Intent serverIntent = new Intent(getApplicationContext(), DeviceListActivity.class);
+                        startActivityForResult(serverIntent, REQUEST_CONNECT_DEVICE_SECURE);
                     }
                 }
             }
         });
+        btn_connect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
+
     //if permission granted or not nresult
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -63,10 +93,117 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == 12) {
             //check if bluetooth granted ( if reslt code = 0 then no )
             if (resultCode != 0) {
-                Toast.makeText(this, "show availaible ppl", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Access Granted", Toast.LENGTH_SHORT).show();
+                Intent serverIntent = new Intent(getApplicationContext(), DeviceListActivity.class);
+                startActivityForResult(serverIntent, REQUEST_CONNECT_DEVICE_SECURE);
             } else {
                 Toast.makeText(this, "The app requires bluetooth permission", Toast.LENGTH_SHORT).show();
             }
+        } else if (requestCode == 1) {
+            if (resultCode == Activity.RESULT_OK) {
+                connectDevice(data, true);
+            }
         }
     }
+
+    /**
+     * Establish connection with other device
+     *
+     * @param data   An {@link Intent} with {@link DeviceListActivity#EXTRA_DEVICE_ADDRESS} extra.
+     * @param secure Socket Security type - Secure (true) , Insecure (false)
+     */
+    private void connectDevice(Intent data, boolean secure) {
+        if (mChatService == null) {
+            // Initialize the BluetoothChatService to perform bluetooth connections
+            mChatService = new BluetoothChatService(getApplicationContext(), mHandler);
+        }
+
+
+        // Get the device MAC address
+        Bundle extras = data.getExtras();
+        if (extras == null) {
+            return;
+        }
+        String address = extras.getString(DeviceListActivity.EXTRA_DEVICE_ADDRESS);
+        // Get the BluetoothDevice object
+        BluetoothDevice device = bluetoothAdapter.getRemoteDevice(address);
+        Log.d("dev", device.toString());
+        // Attempt to connect to the device
+        mChatService.connect(device, secure);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mChatService != null) {
+            mChatService.stop();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // Performing this check in onResume() covers the case in which BT was
+        // not enabled during onStart(), so we were paused to enable it...
+        // onResume() will be called when ACTION_REQUEST_ENABLE activity returns.
+        if (mChatService != null) {
+            // Only if the state is STATE_NONE, do we know that we haven't started already
+            if (mChatService.getState() == BluetoothChatService.STATE_NONE) {
+                // Start the Bluetooth chat services
+                mChatService.start();
+            }
+        }
+    }
+
+    /**
+     * The Handler that gets information back from the BluetoothChatService
+     */
+    @SuppressLint("HandlerLeak")
+    private final Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            Context activity = getApplicationContext();
+            switch (msg.what) {
+                case Constants.MESSAGE_STATE_CHANGE:
+                    switch (msg.arg1) {
+                        case BluetoothChatService.STATE_CONNECTED:
+                            Toast.makeText(activity, "Connected to " + mConnectedDeviceName, Toast.LENGTH_SHORT).show();
+                            break;
+                        case BluetoothChatService.STATE_CONNECTING:
+                            Toast.makeText(activity, "Connecting", Toast.LENGTH_SHORT).show();
+                            break;
+                        case BluetoothChatService.STATE_LISTEN:
+                        case BluetoothChatService.STATE_NONE:
+                            Toast.makeText(activity, "Not Connected", Toast.LENGTH_SHORT).show();
+                            break;
+                    }
+                    break;
+                case Constants.MESSAGE_WRITE:
+                    byte[] writeBuf = (byte[]) msg.obj;
+                    // construct a string from the buffer
+                    String writeMessage = new String(writeBuf);
+                    break;
+                case Constants.MESSAGE_READ:
+                    byte[] readBuf = (byte[]) msg.obj;
+                    // construct a string from the valid bytes in the buffer
+                    String readMessage = new String(readBuf, 0, msg.arg1);
+                    break;
+                case Constants.MESSAGE_DEVICE_NAME:
+                    // save the connected device's name
+                    mConnectedDeviceName = msg.getData().getString(Constants.DEVICE_NAME);
+                    if (null != activity) {
+                        Toast.makeText(activity, "Connected to "
+                                + mConnectedDeviceName, Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+                case Constants.MESSAGE_TOAST:
+                    if (null != activity) {
+                        Toast.makeText(activity, msg.getData().getString(Constants.TOAST),
+                                Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+            }
+        }
+    };
 }
